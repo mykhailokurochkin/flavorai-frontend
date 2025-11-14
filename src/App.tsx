2428f207b0b7c791549498e13ab046fc7516271a
Mykhailo <kurochkinmy@gmail.com>
2025-11-14 11:13:48 +0200

    feat: initialize basic react project and tailwind setup, removed default structure

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+const App = () => {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-100">
+      <h1 className="text-5xl font-bold text-blue-600">
+        Hello Tailwind
+      </h1>
+    </div>
+  )
+}
+
+export default App
